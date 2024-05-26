4638b3f2c993a298db6b376ac870ed4b6643ef28
Kavitha <kavithae085@gmail.com>
2024-05-26 04:34:10 -0700

    Updated successfully

---
+++ git_auto.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/chitterlings/azureplaceholder.git C:\Users\Manju Sai\Desktop
+
+# Navigate into the cloned repository folder
+cd C:\Users\Manju Sai\Desktop
+
+# Add files to the staging area
+git add .
+
+# Commit changes with a commit message
+git commit -m "Updated successfully"
+
+# Push changes to the remote repository
+git push azureplaceholder main
