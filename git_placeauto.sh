1ef454faf75477f4868ae2f7686267b65099e52c
Kavitha <kavithae085@gmail.com>
2024-05-26 07:14:55 -0700

    Automated commit

---
+++ git_placeauto.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clone repository
+git clone https://github.com/chitterlings/azure.git C:/Users/Manju Sai
+
+# Change directory to the cloned repository
+cd C:/Users/Manju Sai
+
+# Make changes to the files (optional)
+
+# Add all changes
+git add .
+
+# Commit changes
+git commit -m "Automated commit"
+
+# Push changes to the remote repository
+git push origin master
